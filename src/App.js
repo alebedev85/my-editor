02b7b0584f99ccdfee5462897487159febbff59c
Andrey Lebedev <alebedev424@gmail.com>
2023-10-17 15:17:12 +0300

    feater: добавил вывод тектса на экран

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
+import { convertToHTML } from 'draft-convert';
+import DOMPurify from 'dompurify';
 
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
@@ -9,13 +11,24 @@ function App() {
   const [editorState, setEditorState] = useState(
     () => EditorState.createEmpty(),
   );
+  const [convertedContent, setConvertedContent] = useState(null);
+
+  useEffect(() => {
+    let html = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(html);
+  }, [editorState]);
+
+  function createMarkup(html) {
+    return {
+      __html: DOMPurify.sanitize(html)
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         Rich Text Editor Example
       </header>
-
       <Editor
         editorState={editorState}
         onEditorStateChange={setEditorState}
@@ -23,6 +36,10 @@ function App() {
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
       />
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={createMarkup(convertedContent)}>
+      </div>
     </div>
   )
 }
